cd4d4aa1f8ac449ec92db756ebe5ca8318d0c3fd
Guilherme Carvalho <guilhermediascarvalhooo@gmail.com>
2022-02-18 15:39:34 -0300

    First Commit

---
+++ src/com/dio/Main.java
@@ -0,0 +1,16 @@
+package com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.soma(10,2);
+
+        calculadora.divisao(25, 5);
+
+        calculadora.subtracao(40, 15);
+
+        calculadora.multiplicacao(9, 3);
+    }
+}
---
+++ src/com/dio/OperacaoMatematica.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public interface OperacaoMatematica {
+     void soma(double num1, double num2);
+
+     void subtracao(double num1, double num2);
+
+     void divisao(double num1, double num2);
+
+    void multiplicacao(double num1, double num2);
+
+
+}
